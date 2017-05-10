f1d34a7a66ca2b551cc7efec8625ca181dfc6884
Jun Huh <juan.huh@hotmail.com>
2017-05-10 19:47:15 +0200

    Added material ui + cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, {Component} from "react";
 import "./App.css";
 import axios from "axios";
-
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
 const AWSLambdaApiURL = "https://y1ye4cr14g.execute-api.eu-central-1.amazonaws.com/prod/aws-lambda-twilio-prod-sendText";
+
+const styles = {
+	wrapper: {
+		display: 'block',
+		flexWrap: 'wrap'
+	},
+	button: {
+		margin: 10
+	}
+};
+injectTapEventPlugin();
 
 class App extends Component {
 	
@@ -16,11 +30,11 @@ class App extends Component {
 	}
 	
 	onPhoneNumberChanged(e) {
-		this.setState({to: Object.assign({}, this.state.to, {to: e.target.value})})
+		this.setState({to: e.target.value})
 	}
 	
 	onMessageChanged(e) {
-		this.setState({message: Object.assign({}, this.state.message, {message: e.target.value})})
+		this.setState({message: e.target.value})
 	}
 	
 	sendSmsMessage() {
@@ -28,7 +42,7 @@ class App extends Component {
 		const payload = {
 			to: this.state.to,
 			message: this.state.message
-		}
+		};
 		
 		console.log("Sending sms with payload " + JSON.stringify(payload));
 		
@@ -47,16 +61,20 @@ class App extends Component {
 	
 	render() {
 		return (
-			<div className="App">
-				<h1>A Serverless API deployed in AWS Lambda made by Jun Huh</h1>
-				<div class="inputs">
-					<input id="phone-number" placeholder="Phone Number" onChange={this.onPhoneNumberChanged.bind(this)}/>
-					<input id="text" placeholder="Input your text message" onChange={this.onMessageChanged.bind(this)}/>
+			<MuiThemeProvider>
+				<div className="App">
+					<h1>A Serverless API deployed in AWS Lambda made by Jun Huh</h1>
+					<div style={styles.wrapper}>
+						<TextField floatingLabelText="Phone Number... (+460701712323)" name="Phone Number" value={this.state.to} onChange={this.onPhoneNumberChanged.bind(this)}/>
+					</div>
+					<div style={styles.wrapper}>
+						<TextField hintText="Text message..." multiLine={true} fullWidth={false} value={this.state.message} onChange={this.onMessageChanged.bind(this)}/>
+					</div>
+					<div style={styles.button}>
+						<RaisedButton primary={true} onClick={this.sendSmsMessage.bind(this)} label="Send SMS"/>
+					</div>
 				</div>
-				<div class='api-actions'>
-					<button id="send-text" class="btn" onClick={this.sendSmsMessage.bind(this)}>Send Text</button>
-				</div>
-			</div>
+			</MuiThemeProvider>
 		);
 	}
 }
